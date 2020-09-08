fb68d2b48eb151d68aaa909b5060d7bc1c382b0f
Francisco Silva <cm.franciscosilva@gmail.com>
2020-09-08 22:10:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ y_in = []
 x_out = []
 y_out = []
 
-for i in range(10000000):
+for i in range(10000000): #choose number of iterations  
 
-    x = random.uniform(-1, 1)
-    y = random.uniform(-1, 1)
+    x = random.uniform(-1, 1) #generate x coordinate
+    y = random.uniform(-1, 1) #generate y coordinate
 
-
-    dist = math.sqrt(pow(x, 2) + pow(y, 2))
+    dist = math.sqrt(pow(x, 2) + pow(y, 2)) #get the distance from the origin
 
     total_points += 1
     if dist <= 1:
@@ -31,6 +30,9 @@ plt.plot(x_out, y_out, 'bo')
 plt.axis([-1, 1, -1, 1])
 plt.show()
 
-pi = 4* (points_in / total_points)
+# square_area = (2*r)^2
+# circle_area = pi*r^2
+
+pi = 4* (points_in / total_points) #get the pi value by the areas relation
 print(pi)
 
